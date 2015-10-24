1f1f5fd7eb630487853e5fa6c80f079ffc314cb4
rcarcamoc <ricardo.carcamo.cofre@gmail.com>
2015-10-24 12:21:27 -0300

    carga inicial
    se carga modulo de login

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')-> truncate();
+        factory(App\User::class)->create([
+            'name' => 'ricardo',
+            'roles' => 'admin',
+            'email' => 'ricardo@carcamo.com',
+            'password' =>   bcrypt('pass')
+        ]);
+        factory(App\User::class,49)->create();
+
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">welcome</div>
+                    <div class="panel-body">
+                        <p>welcome</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
